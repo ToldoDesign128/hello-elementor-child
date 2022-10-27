593b44cfdb59d8cc573add5e52d5e81bbae2c2cf
Federico <fede.toldo96@gmail.com>
2022-10-27 10:41:02 +0200

    init
    init

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function hello_elementor_child_enqueue_scripts() {
+	wp_enqueue_style(
+		'hello-elementor-child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[
+			'hello-elementor-theme-style',
+		],
+		'1.0.0'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
+
+?>
