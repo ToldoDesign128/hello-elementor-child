efc87659e223b54e4a085e732b0ae1e2388f990f
matteobruschetti <matteobruschetti@gmail.com>
2022-11-04 13:08:30 +0100

    minified css
    npm run watch

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,21 @@
 // webpack.mix.js
 
 let mix = require('laravel-mix');
+let minifier = require('minifier');
 
 mix
-    .sass('assets/scss/style.scss', 'style.css')
-    .options({
-        watchOptions: {
-            ignored: /node_modules/
-        }
-    });
+.sass('assets/scss/style.scss', 'style.css')
+// outputStyle: 'nested',
+
+.options({
+    watchOptions: {
+        ignored: /node_modules/
+    }
+});
+
+mix.then(() => {
+    minifier.minify('style.css', '')
+});
 
 if (process.env.MIX_NOTIFICATIONS == 'false') {
     mix.disableNotifications();
