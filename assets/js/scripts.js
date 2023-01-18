4bd15829ee042993447b710f6023949dc901e533
Federico <fede.toldo96@gmail.com>
2023-01-18 16:04:08 +0100

    fix footer
    fix footer

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,59 +1,59 @@
 // Dark mode storage
 jQuery(window).on("load", function () {
-   // local storage key
-   const currentDarkMode = localStorage.getItem("dark-mode");
-   if (currentDarkMode) {
-      jQuery("body").addClass(currentDarkMode);
-      if (currentDarkMode === "dark-mode-on") {
-         jQuery("#dark-mode-trigger").prop("checked", true);
-         jQuery(".dark-mode-label").addClass("dark-label");
-      } else {
-         // default theme style
-      }
-   }
-   jQuery(".dark-mode-label").on("change", switchColorTheme);
+  // local storage key
+  const currentDarkMode = localStorage.getItem("dark-mode");
+  if (currentDarkMode) {
+    jQuery("body").addClass(currentDarkMode);
+    if (currentDarkMode === "dark-mode-on") {
+      jQuery("#dark-mode-trigger").prop("checked", true);
+      jQuery(".dark-mode-label").addClass("dark-label");
+    } else {
+      // default theme style
+    }
+  }
+  jQuery(".dark-mode-label").on("change", switchColorTheme);
 });
 // Dark mode toggle class
 function switchColorTheme(e) {
-   jQuery(this).toggleClass("dark-label");
-   removeDarkClasses();
-   if (e.target.checked) {
-      // dark mode on
-      jQuery("body").addClass("dark-mode-on");
-      localStorage.setItem("dark-mode", "dark-mode-on");
-      jQuery("#dark-mode-trigger").prop("checked", true);
-   } else {
-      // dark mode of
-      jQuery("body").addClass("dark-mode-off");
-      localStorage.setItem("dark-mode", "dark-mode-off");
-      jQuery("#dark-mode-trigger").prop("checked", false);
-   }
+  jQuery(this).toggleClass("dark-label");
+  removeDarkClasses();
+  if (e.target.checked) {
+    // dark mode on
+    jQuery("body").addClass("dark-mode-on");
+    localStorage.setItem("dark-mode", "dark-mode-on");
+    jQuery("#dark-mode-trigger").prop("checked", true);
+  } else {
+    // dark mode of
+    jQuery("body").addClass("dark-mode-off");
+    localStorage.setItem("dark-mode", "dark-mode-off");
+    jQuery("#dark-mode-trigger").prop("checked", false);
+  }
 }
 // Reset initial Dark mode state
 function removeDarkClasses() {
-   jQuery("body").removeClass("dark-mode-on");
-   jQuery("body").removeClass("dark-mode-off");
+  jQuery("body").removeClass("dark-mode-on");
+  jQuery("body").removeClass("dark-mode-off");
 }
+
+jQuery(document).ready(function () {
+  //FAQs widget
+  jQuery(".faq-container").click(function () {
+    jQuery(this).toggleClass("faq-active");
+  });
+
+  //Alert widget
+  jQuery(".close-alert").click(function () {
+    jQuery(this)
+      .parent()
+      .parent()
+      .parent()
+      .parent()
+      .parent(".fbk-cw-alert")
+      .addClass("closed-alert");
+  });
+});
 
 // Open list footer
 jQuery(".footer-title").click(function () {
-   jQuery(this).next().children().toggleClass("show-item");
-});
-
-jQuery(document).ready(function () {
-   //FAQs widget
-   jQuery(".faq-container").click(function () {
-      jQuery(this).toggleClass("faq-active");
-   });
-
-   //Alert widget
-   jQuery(".close-alert").click(function () {
-      jQuery(this)
-         .parent()
-         .parent()
-         .parent()
-         .parent()
-         .parent(".fbk-cw-alert")
-         .addClass("closed-alert");
-   });
+  jQuery(this).next().children().toggleClass("show-footer-item");
 });
